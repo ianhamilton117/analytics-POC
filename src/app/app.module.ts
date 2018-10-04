98506c836bb51da89d5c79b4987e8b52ae6dbdaa
Ian Hamilton <ian.hamilton117@gmail.com>
2018-10-04 14:46:50 -0700

    Adding support for Google Analytics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { RouteOneComponent } from './route-one/route-one.component';
+import { RouteTwoComponent } from './route-two/route-two.component';
+
+const appRoutes: Routes = [
+  { path: '', component: RouteOneComponent},
+  { path: 'routeone', component: RouteOneComponent },
+  { path: 'routetwo', component: RouteTwoComponent },
+  { path: '**', redirectTo: '/routetwo'}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RouteOneComponent,
+    RouteTwoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
